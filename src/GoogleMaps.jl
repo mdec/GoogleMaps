1e7a3f07d978e4183c2cb3a071a086cc1d68e16c
Mason DeCamillis <md2477@gmail.com>
2014-02-18 23:14:13 -0500

    Adding getMap function, still incomplete until I figure out how to save PNG files

--- src/GoogleMaps.jl
+++ src/GoogleMaps.jl
@@ -1,10 +1,21 @@
 module GoogleMaps
 
+using Requests
+
 export LatLng
 
 type LatLng
     lat::Float64
     lng::Float64
+end
+
+
+function getMap(
+    center::LatLng,
+    zoom::Int)
+
+    urlBase = "http://maps.googleapis.com/maps/api/staticmap"
+
 end
 
 # package code goes here
