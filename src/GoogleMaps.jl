b1d0d17059f73a673641561de6a9112b1e4ce448
Mason DeCamillis <md2477@gmail.com>
2014-02-18 23:47:18 -0500

    geocode function working for string addresses with no spaces

--- src/GoogleMaps.jl
+++ src/GoogleMaps.jl
@@ -1,14 +1,25 @@
 module GoogleMaps
 
-using Requests
+using Requests, JSON
 
-export LatLng
+export LatLng, geocode
 
 type LatLng
     lat::Float64
     lng::Float64
 end
 
+function geocode(
+    address::String)
+
+    geocodeResponse = get("https://maps.googleapis.com/maps/api/geocode/json?address=$address&sensor=false&key=AIzaSyAEGq5zoIMANJa-5Q2Ux98rRjMx_ShlLtI")
+    parsedResponse = JSON.parse(geocodeResponse.data)
+
+    location = parsedResponse["results"][1]["geometry"]["location"]
+    geocodeLatLng = LatLng(location["lat"], location["lng"])
+
+    return(geocodeLatLng)
+end
 
 function getMap(
     center::LatLng,
