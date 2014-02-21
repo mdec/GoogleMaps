edccc1c6af20a43caddc01643df1767b3aaaa7a8
Mason DeCamillis <md2477@gmail.com>
2014-02-21 15:46:36 -0500

    Added Geocode type, factored out several functions from the geocode API call process. Geocode now consists of a center (LatLng) and a name (String).

--- src/maps.jl
+++ src/maps.jl
@@ -1,35 +1,60 @@
-function getLatLngFromGeocode(
+using JSON, Requests
+include ("types.jl")
+
+function callGeocodeAPI(
+    url::String)
+
+    rawResponse = get(url)
+    parsedResponseData = JSON.parse(rawResponse.data)
+
+    return(parsedResponseData)
+end
+
+function changeSpaceToPlus!(
+    str::String)
+
+    str = replace(str, r" +", "+")
+    return(str)
+end
+
+function extractGeocodeInfo(
     geocodeResponse::Dict{String, Any})
 
-    location::Dict = geocodeResponse["results"][1]["geometry"]["location"]
-    latLng::LatLng = LatLng(location["lat"], location["lng"])
+    location = geocodeResponse["results"][1]["geometry"]["location"]
+    center = LatLng(location["lat"], location["lng"])
 
-    return(latLng)
+    name = geocodeResponse["results"][1]["formatted_address"]
+
+    info = Geocode(center, name)
+    return(info)
 end
 
-function replaceAddressSpaces!(
+function getGeocodeURL(
     address::String)
 
-    address = replace(address, r" +", "+")
-    return(address)
+    url = "https://maps.googleapis.com/maps/api/geocode/json?address=$address&sensor=false&key=AIzaSyAEGq5zoIMANJa-5Q2Ux98rRjMx_ShlLtI"
+    return(url)
 end
 
+function geocode(
+    zip::Int)
 
+
+end
 
 function geocode(
     address::String)
 
-    address = replaceAddressSpaces!(address)
-    rawResponse = get("https://maps.googleapis.com/maps/api/geocode/json?address=$address&sensor=false&key=AIzaSyAEGq5zoIMANJa-5Q2Ux98rRjMx_ShlLtI")
-    parsedResponseData = JSON.parse(rawResponse.data)
-
-    return(parsedResponseData)
+    address = changeSpaceToPlus!(address)
+    geocodeURL = getGeocodeURL(address)
+    geocodeResponse = callGeocodeAPI(geocodeURL)
+    geocodeResults = extractGeocodeInfo(geocodeResponse)
+    return(geocodeResults)
 end
 
 function getMap(
     center::LatLng,
     zoom::Int)
 
-    urlBase = "http://maps.googleapis.com/maps/api/staticmap"
 
 end
--- src/types.jl
+++ src/types.jl
@@ -2,3 +2,9 @@ type LatLng
     lat::Float64
     lng::Float64
 end
+
+type Geocode
+    #bounds::Array{LatLng}
+    center::LatLng
+    name::String
+end
